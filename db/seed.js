7510ed196d97d3f1d8036ced6a46e29eb758e35e
zuly11 <zuly.rod.11@gmail.com>
2023-01-10 10:08:09 -0600

    added seeding

--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,65 @@
-// grab our client with destructuring from the export in index.js
-const { client } = require("./index");
-async function testDB() {
+const { client, getAllUsers } = require("./index");
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
+        DROP TABLE IF EXISTS users;
+      `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          password varchar(255) NOT NULL
+        );
+      `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
+}
+
+async function rebuildDB() {
   try {
     client.connect();
 
-    const { rows } = await client.query(`SELECT * FROM users;`);
-    console.log(rows);
+    await dropTables();
+    await createTables();
   } catch (error) {
-    console.error(error);
-  } finally {
-    client.end();
+    throw error;
   }
 }
-testDB();
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    const users = await getAllUsers();
+    console.log("getAllUsers:", users);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
