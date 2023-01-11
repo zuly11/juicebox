98fa517a4585750b0731f4f473b39ffbe68409f6
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-01-11 11:46:52 -0500

    added sandra and glamgal, cleaned up createUser and createInitialUsers

--- db/index.js
+++ db/index.js
@@ -15,15 +15,17 @@ async function getAllUsers() {
 
 async function createUser({ username, password }) {
   try {
-    const result = await client.query(
+    const { rows } = await client.query(
       `
       INSERT INTO users(username, password)
-      VALUES ($1, $2);
+      VALUES ($1, $2)
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
     `,
       [username, password]
     );
 
-    return result;
+    return rows;
   } catch (error) {
     throw error;
   }
--- db/seed.js
+++ db/seed.js
@@ -5,8 +5,9 @@ async function createInitialUsers() {
     console.log("Starting to create users...");
 
     const albert = await createUser({ username: 'albert', password: 'bertie99' });
-
-    console.log(albert);
+    const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
+    const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
+    console.log(albert, sandra, glamgal);
 
     console.log("Finished creating users!");
   } catch(error) {
