956b86848f48ca525d77139feaed69cacccfe6e0
zuly11 <zuly.rod.11@gmail.com>
2023-01-18 18:28:48 -0600

    added and installed .env file

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+require("dotenv").config();
+
+// remove this once you confirm it works
+//console.log(process.env.JWT_SECRET);
+// like, seriously. go delete that!
+
+// EVERYTHING ELSE
+
 const PORT = 3000;
 const express = require("express");
 const server = express();
